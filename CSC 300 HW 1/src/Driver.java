e38be49950f29890f95c580bfc9305f40b9afb18
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2023-08-30 13:16:13 -0500

    Add files via upload

---
+++ CSC 300 HW 1/src/Driver.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+
+public class Driver {
+	public static void main(String Args[]) {
+		
+		Salesman[] s = createMen(100,100);
+		results(getTopFive(s));
+	}
+	
+	
+	static Salesman[] createMen(int num, int randRange) {
+		Salesman[] men = new Salesman[num];
+		
+		for(int i = 0; i < num; i++) {
+			Random r =  new Random();
+    		int x = r.nextInt(randRange);
+			men[i] = new Salesman("Salesman " + Integer.toString(i), x);
+		}
+		
+		return men;
+	}
+	
+	static Salesman[] getTopFive(Salesman[] allSales){
+
+		Salesman[] topFive = createMen(100,1);
+		
+		for(int i = 0; i < allSales.length; i++) {
+			for(int j = 0; j < topFive.length; j++) {
+				if(allSales[i].getSales() > topFive[j].getSales()) {
+					
+					topFive = adjust(j,topFive);
+					topFive[j] = allSales[i];
+					break;
+				}
+			}
+		}
+		
+		return topFive;
+
+	}
+	
+	static Salesman[] adjust(int index, Salesman[] s) {
+		
+		for(int i = s.length - 1; i > index; i--) {
+			s[i] = s[i-1];
+		}
+		return s;
+	}
+
+	static void results(Salesman[] s) {
+		
+		for(int i = 0; i < s.length; i++) {
+			System.out.println(s[i].getName() + ": " + s[i].getSales());
+		}
+	}
+}
---
+++ CSC 300 HW 1/src/Salesman.java
@@ -0,0 +1,21 @@
+
+class Salesman{
+
+	String name;
+
+	float sales;
+	
+	Salesman(String name, float sales){
+		this.name = name;
+		this.sales = sales;
+	}
+	
+	public float getSales() {
+		return this.sales;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+}
